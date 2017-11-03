1597445f3fa607765625f74e5cc6ef8fd938e88c
LavenderIris <pchung@gmail.com>
2017-11-02 23:41:27 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Switchboard';
+  buttons = [['OFF', 'red'], ['ON', 'green'], ['OFF', 'red'], ['OFF', 'red'], ['OFF', 'red'], ['OFF', 'red'], 
+    ['OFF', 'red'], ['OFF', 'red'], ['OFF', 'red'], ['OFF', 'red']];
+
+  toggle_button(num) {
+    if (this.buttons[num][0] === 'OFF') {
+      this.buttons[num][0] = 'ON';
+      this.buttons[num][1] = 'green';
+    } else if (this.buttons[num][0] === 'ON') {
+      this.buttons[num][0] = 'OFF';
+      this.buttons[num][1] = 'red';
+    }
+  }
 }
